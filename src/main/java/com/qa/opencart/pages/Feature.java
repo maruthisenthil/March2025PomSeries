43fdddbe9c735f2fe800178a6b1ac20cffdc68b1
maruthisenthil <senthilbdpm@gmail.com>
2025-09-09 06:59:40 +0530

    added f2 feature

--- src/main/java/com/qa/opencart/pages/Feature.java
+++ src/main/java/com/qa/opencart/pages/Feature.java
@@ -3,5 +3,11 @@ package com.qa.opencart.pages;
 public class Feature {
 
 	int featureId;
+	public void f1() {
+		System.out.println("Feature 1");
+	}
 	
+	public void f2() {
+		System.out.println("Feature 2");
+	}
 }
